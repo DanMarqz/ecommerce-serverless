69d4c81a313ed9a6d0fc94a6390fa7d6cb3b49a0
Daniel <danielarturo.marquez@gmail.com>
2021-12-15 09:17:56 -0400

    ProductCard altered

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Product from './Product';
 
 const ProductCard = ({product}) => {
   return (
     <article className="card p-4">
       <header>
-        <h2 className="font-bold text-2xl">{ product.name }</h2>
+        {/* <h2 className="font-bold text-2xl">{ product.name }</h2> */}
+        <Product></Product>
       </header>
     </article>
   )
